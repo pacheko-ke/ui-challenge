924ee1cef761d65f84ed917a43e96d4734d08dbf
Pacheko <patrickoluochex@gmail.com>
2025-03-15 11:15:15 +0300

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ function App() {
 
 
   return (
-    <div className="flex flex-col text-7xl -ml-10 mr-20  mt-26 rotate-90 ">
-      <ul className="divide-black divide-y-2 flex  flex-col px-10">
+    <div className="flex text-7xl flex-col mt-26 rotate-90 ">
+      sss
+      <ul className="divide-black divide-y-2 flex  flex-col  text-gray-500 gap-0.5">
         <li>About</li>
-        
-        <li>Services</li>
+
+        <li>Services </li>
         <li>Projects</li>
-        
+
         <li>Contact</li>
-        
+
       </ul>
+
+
+
+      
     </div>
   )
 }

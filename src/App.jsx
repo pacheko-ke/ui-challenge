ae4ccde0edbd53176946579df07e46cb093c0042
Pacheko <patrickoluochex@gmail.com>
2025-03-15 11:08:30 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,17 @@ function App() {
 
 
   return (
-    <>
-     
-    </>
+    <div className="flex flex-col text-7xl -ml-10 mr-20  mt-26 rotate-90 ">
+      <ul className="divide-black divide-y-2 flex  flex-col px-10">
+        <li>About</li>
+        
+        <li>Services</li>
+        <li>Projects</li>
+        
+        <li>Contact</li>
+        
+      </ul>
+    </div>
   )
 }
 

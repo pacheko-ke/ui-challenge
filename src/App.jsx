75292d601670ef066c060fc64042b0696d28623e
Pacheko <patrickoluochex@gmail.com>
2025-03-15 11:38:28 +0300

    deploying to github

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ function App() {
 
 
   return (
-    <div className="flex text-7xl flex-col mt-26 rotate-90 ">
-      sss
-      <ul className="divide-black divide-y-2 flex  flex-col  text-gray-500 gap-0.5">
+    <>
+    <div className="flex text-7xl justify-between mx-2 mt-6">
+
+      <ul className="divide-black divide-y-2 flex  flex-col  text-gray-500 gap-0.5  ml-6 rotate-90">
+        
         <li>About</li>
 
         <li>Services </li>
@@ -15,10 +17,14 @@ function App() {
 
       </ul>
 
-
-
+  
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-16 ">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+      </svg>
       
     </div>
+       
+      </>
   )
 }
 
